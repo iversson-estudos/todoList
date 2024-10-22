73c0c6d2e0c73ee09314b2c9b61d2c20edbf8c71
iversson-estudos <i versson.estudos@gmail.com>
2024-10-22 12:48:19 -0300

    working on the localstorage logic behind the objects

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import { Todo } from "./classes/todo.js";
 
 
 
+
+
+
+
--- src/views/mainView.js
+++ src/views/mainView.js
@@ -1,2 +1,21 @@
 
 import { List } from "../classes/list.js"
+
+
+//GETS MAIN CONTAINER AND EMPTIES IT
+const main = document.getElementById('main');
+main.innerHTML=`
+            <div id="left-container">
+                <button id="newListBtn">NEW LIST</button>
+                <div id="listContainer"></div>
+            </div>
+            <div id="main-container">
+            </div>`;
+
+//INSERT LISTS ONTO LIST CONTAINER
+const listContainer=document.getElementById('listContainer');
+
+
+
+
+
