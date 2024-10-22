80bb144b3eb0b16542c14f8b41598e8423c7e97b
iversson-estudos <i versson.estudos@gmail.com>
2024-10-22 15:19:30 -0300

    will need to reinstance the lists when retrieving from localStorage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import "./css/styles.css";
 import { List } from "./classes/list.js";
 import { Todo } from "./classes/todo.js";
+import { mainView } from "./views/mainView.js";
 
+
+
+//LISTS FOR TEST, NEED TO CREATE AN DEFAULT ONE WHEN FINISHED
+const listsArray = [
+    new List('Apple', '2024-11-01'),   // Format: yyyy-MM-dd
+    new List('Microsoft', '2025-02-14'),
+    new List('Amazon', '2024-09-23'),
+    new List('Netflix', '2025-07-05'),
+    new List('Tesla', '2024-12-12'),
+    new List('Facebook', '2025-03-20'),
+    new List('Samsung', '2024-06-11'),
+    new List('Sony', '2025-08-30'),
+    new List('Intel', '2024-10-25'),
+    new List('IBM', '2025-01-17')
+];
+
+//VARIABLE THAT HOLDS ALL THE LISTS ON LOCALSTORAGE
+localStorage.setItem('listsArray',JSON.stringify(listsArray));
+
+mainView();
 
 
 
--- src/views/mainView.js
+++ src/views/mainView.js
@@ -1,8 +1,8 @@
 
 import { List } from "../classes/list.js"
 
-
-//GETS MAIN CONTAINER AND EMPTIES IT
+function mainView(){
+//GETS MAIN CONTAINER AND ORGANIZES IT TO ACCOMODATE THE VIEW
 const main = document.getElementById('main');
 main.innerHTML=`
             <div id="left-container">
@@ -13,7 +13,31 @@ main.innerHTML=`
             </div>`;
 
 //INSERT LISTS ONTO LIST CONTAINER
-const listContainer=document.getElementById('listContainer');
+const listContainer = document.getElementById('listContainer');
+
+const arrayOfLists = JSON.parse(localStorage.getItem('listsArray'));
+console.log(arrayOfLists);
+
+arrayOfLists.forEach(element => {
+    const divList = document.createElement('div');
+    const pName = document.createElement('p');
+    const pDueDate = document.createElement('p');
+    const pCreationDate = document.createElement('p');
+
+    pName.textContent = 'Name: '+element.name;
+    pDueDate.textContent= 'Due Date: '+element.dueDate;
+    pCreationDate.textContent= 'Due Date: '+element.dateOfCreation;
+
+    divList.appendChild(pName);
+    divList.appendChild(pDueDate);
+    divList.appendChild(pCreationDate);
+
+    listContainer.appendChild(divList);
+});
+
+}
+
+export { mainView };
 
 
 
